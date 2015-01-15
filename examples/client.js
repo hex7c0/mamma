a6b68485e8a3b93b5a4fe7f5584cfa047d40c876
hex7c0 <carniellifrancesco@gmail.com>
2015-01-15 17:35:57 +0100

    add examples

---
+++ examples/client.js
@@ -0,0 +1,25 @@
+'use strict';
+/**
+ * @file server example
+ * @module mamma
+ * @package mamma
+ * @subpackage examples
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+  var mamma = require('..'); // use require('mamma')
+} catch (MODULE_NOT_FOUND) {
+  console.error(MODULE_NOT_FOUND);
+  process.exit(1);
+}
+
+mamma.createClient({
+  port: 3000
+}, 'child 1');
